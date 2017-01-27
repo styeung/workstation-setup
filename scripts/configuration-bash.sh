ddc6e8b2c1a04f20a840e12ec174e13c37a8de4a
Sai To Yeung <saito@saito.com>
2017-01-27 01:31:35 -0500

    Updated configuration-bash.sh

--- scripts/configuration-bash.sh
+++ scripts/configuration-bash.sh
@@ -8,7 +8,7 @@ git clone https://github.com/Bash-it/bash-it.git ~/.bash_it
 cp files/add_user_initials_to_git_prompt_info.bash ~/.bash_it/custom
 cp files/bobby_pivotal/bobby_pivotal.theme.bash ~/.bash_it/themes/bobby/bobby.theme.bash
 ~/.bash_it/install.sh
-source ~/.bash_profile
+source ~/.bashrc
 bash-it enable completion git
 bash-it enable plugin ssh
 bash-it enable plugin rbenv
