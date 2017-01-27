ae2f97a1aa59ab107ab3a23d3a5545a222b684b6
Sai To Yeung <saito@saito.com>
2017-01-27 01:32:29 -0500

    Updated configurations.sh

--- scripts/configurations.sh
+++ scripts/configurations.sh
@@ -1,37 +1,7 @@
 echo
-# prereqs
-brew install tccutil
-brew tap OJFord/formulae
-brew install loginitems
-
-echo "Configuring iTerm"
-cp files/com.googlecode.iterm2.plist ~/Library/Preferences
-
-echo "Configuring ShiftIt"
-sudo tccutil --insert "org.shiftitapp.ShiftIt" # Enable Accessibility Settings
-loginitems -a "ShiftIt" # Start on login
-open /Applications/ShiftIt.app
-
-echo "Configuring FlyCut"
-loginitems -a "Flycut" # Start at login
-open /Applications/Flycut.app
-
-echo
 echo "Installing vim configuration"
 pushd ~/
 rm -rf ~/.vim
 git clone https://github.com/pivotal/vim-config.git ~/.vim
 ~/.vim/bin/install
 popd
-
-echo
-echo "Setting up Pivotal IDE preferences..."
-pushd ~/workspace
-rm -rf pivotal_ide_prefs
-git clone https://github.com/pivotal/pivotal_ide_prefs.git
-pushd pivotal_ide_prefs/cli/
-./bin/ide_prefs install --ide=intellij
-./bin/ide_prefs install --ide=rubymine
-popd
-popd
-
