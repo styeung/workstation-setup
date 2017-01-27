d896f44a5ad752f783c5003f46bd804e379da7be
Sai To Yeung <saito@saito.com>
2017-01-27 01:29:31 -0500

    Updated cloud.sh

--- scripts/cloud.sh
+++ scripts/cloud.sh
@@ -1,4 +1,7 @@
 echo
 echo "Installing Cloud Foundry Command-line Interface"
-brew tap cloudfoundry/tap
-brew install cf-cli
+
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+sudo apt-get update
+sudo apt-get install cf-cli
