fdf416451912507288da2063a68707002f4995c7
Sai To Yeung <saito@saito.com>
2017-01-27 01:29:15 -0500

    Updated applications-common.sh

--- scripts/applications-common.sh
+++ scripts/applications-common.sh
@@ -7,38 +7,10 @@ echo "Installing applications"
 
 # Utilities
 
-brew cask install flux
-brew cask install flycut
-brew cask install shiftit
-brew cask install dash
-brew cask install postman
-brew cask install google-drive
-brew install the_silver_searcher
+sudo apt-get install silversearcher-ag
 
-# Terminals
-
-brew cask install iterm2
-
-# Browsers
-
-brew cask install google-chrome
-brew cask install firefox
-
-# Communication
-
-brew cask install slack
-brew cask install screenhero
-brew cask install skype
-
-# Text Editors
-
-brew cask install macdown
-brew cask install sublime-text
-brew cask install textmate
-brew cask install macvim
-
-# Emulation tools
-
-brew cask install virtualbox
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
 
 set -e
