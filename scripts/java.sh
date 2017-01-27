0376ac31b863741079626580f61c625d382111c0
Sai To Yeung <saito@saito.com>
2017-01-27 01:23:19 -0500

    Updated java.sh

--- scripts/java.sh
+++ scripts/java.sh
@@ -1,9 +1,8 @@
 echo
 echo "Installing Java Development tools"
-brew cask install java
-brew cask install intellij-idea
-brew install maven
-brew install gradle
-brew install nexus
-brew install jenkins
-brew install springboot
+sudo add-apt-repository ppa:webupd8team/java
+sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
+sudo apt-get update
+sudo apt-get install --assume-yes oracle-java8-installer maven ubuntu-make
+
+umake ide idea-ultimate
