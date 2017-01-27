4f22f7b58cb6b6877a29784556f44159ab7d945c
Sai To Yeung <saito@saito.com>
2017-01-26 23:49:16 -0500

    Updated ruby.sh

--- scripts/ruby.sh
+++ scripts/ruby.sh
@@ -1,12 +1,18 @@
 echo
 echo "Installing Ruby tools and Ruby 2.3.1"
 cp files/.irbrc ~/.irbrc
-brew install readline
-brew install rbenv
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
+source ~/.bash_profile
+
 eval "$(rbenv init -)"
-rbenv install 2.3.1 --skip-existing
-rbenv global 2.3.1
+rbenv install 2.4.0 --skip-existing
+rbenv global 2.4.0
 gem install bundler
 rbenv rehash
-
-brew cask install rubymine
